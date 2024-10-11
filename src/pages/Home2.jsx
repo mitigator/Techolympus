0298140b75cd1094653d8b9efcb1c7b402665658
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2024-10-11 23:55:11 +0530

    bg-fix-2

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -113,7 +113,7 @@ const Home2 = () => {
             <div
             data-aos="fade-in"
             data-aos-duration="1000"
-            className="fixed inset-0 z-0 bg-scroll bg-center bg-no-repeat bg-contain md:bg-fixed md:bg-cover  "
+            className="fixed inset-0 z-0  bg-fixed bg-center bg-no-repeat bg-contain md:bg-fixed md:bg-cover  "
             style={{
                 backgroundImage: `url(${zeus})`,
                 backgroundPosition: 'bottom',
