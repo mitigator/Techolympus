b5285c51dda6382095320be63c30c31d113054a5
mitigator <shubhammishra4623@gmail.com>
2024-10-13 17:15:52 +0530

    Gallery added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {Photography} from './events/Photography';
 import {Treasure} from './events/Treasure';
 import {Uiux} from './events/Uiux';
 import {Gaming} from './events/Gaming';
+import Gallery from './components/Gallery';
 import {AnimatePresence} from 'framer-motion'
 import './animation/Transition.css'
 import "aos/dist/aos.css"
@@ -43,6 +44,7 @@ function App() {
         <Route path="/treasure" element={<Treasure />} />
         <Route path="/uiux" element={<Uiux />} />
         <Route path="/gaming" element={<Gaming />} />
+        <Route path="/gallery" element={<Gallery />} />
         <Route path="*" element={<NotFound />} /> 
       </Routes>
     </BrowserRouter>
