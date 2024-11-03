6ad54aa1da5684358d58dc15a6f8ee0ac37ef77c
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2024-11-03 20:57:57 +0530

    scroll up button

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -528,14 +528,12 @@ const Home2 = () => {
                     href="/assets/Gateways2024-Brochure.pdf" download >Download Brochure</a>
 
             </div>
-            <div className='flex justify-end px-10 py-10 mx-auto'>
-                <div onClick={movetoTop} className='p-2 rounded-full flex bg-[#e7b472] cursor-pointer'>
+            <div className='flex justify-end px-10 py-10 '>
+                <div onClick={movetoTop} className='p-2 bg-[#e7b472]  cursor-pointer'>
                 <svg height={20} width={20} viewBox="0 0 384 512"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>
                 </div>
             </div>
-          
-
-                   
+            
             <Footer />
             </div>
 
