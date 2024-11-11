f6d1b42278fe6033b9a3df3270fd6f246e852a3e
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2024-11-11 14:37:14 +0530

    brochure update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = (activePage) => {
   const [lastScrollY, setLastScrollY] = useState(0);
   const controls = useAnimation();
 
-  const flipBookURL = "https://heyzine.com/flip-book/68b930374e.html"
+  const flipBookURL = "https://heyzine.com/flip-book/98523e19ca.html"
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
